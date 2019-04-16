c3f20b3a5841f173b60242c88c45bcf4d5684687
liuruibinggit <2675247482@qq.com>
2019-04-16 17:29:24 +0800

    新增

--- src/main/java/cn/richinfo/richadmin/RichAdminApplication.java
+++ src/main/java/cn/richinfo/richadmin/RichAdminApplication.java
@@ -2,11 +2,13 @@ package cn.richinfo.richadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //开启总的定时任务
 @EnableScheduling
+@ComponentScan(basePackages = {"cn.richinfo"})
 public class RichAdminApplication {
 
 	public static void main(String[] args) {
