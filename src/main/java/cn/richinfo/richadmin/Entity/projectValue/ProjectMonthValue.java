9c742c35870fade464f3300c56cf3d71104a47e1
liuruibinggit <2675247482@qq.com>
2019-04-17 10:10:01 +0800

    新增

---
+++ src/main/java/cn/richinfo/richadmin/Entity/projectValue/ProjectMonthValue.java
@@ -0,0 +1,15 @@
+package cn.richinfo.richadmin.Entity.projectValue;
+
+import lombok.Data;
+
+/**
+ * Created by LiuRuibing on 2019/4/16 0016.
+ */
+@Data
+public class ProjectMonthValue {
+    private String projectNum;
+    private String projectName;
+    private String workTime;
+    private String dept;
+    private Double value;
+}
