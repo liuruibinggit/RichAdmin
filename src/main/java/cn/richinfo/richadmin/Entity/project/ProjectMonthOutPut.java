f0750db8feaabbbfe57fd730642a3283e40ef987
liuruibinggit <2675247482@qq.com>
2019-04-17 10:06:52 +0800

    新增

---
+++ src/main/java/cn/richinfo/richadmin/Entity/project/ProjectMonthOutPut.java
@@ -0,0 +1,20 @@
+package cn.richinfo.richadmin.Entity.project;
+
+import lombok.Data;
+
+/**
+ * Created by LiuRuibing on 2019/4/16 0016.
+ */
+@Data
+public class ProjectMonthOutPut {
+
+   // private Integer pid;
+
+    private Integer pId;
+
+    private Integer sort;
+
+    private String month;
+
+    private Double monthOutPut;
+}
